e9d71ace78425c3c6791eb623597f4548a2b89bc
thomas maestas <thomasm1.maestas@gmail.com>
2019-05-21 23:30:15 -0600

    ng-atlas

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Welcome to {{name}}</h1>`,
+})
+export class AppComponent  { name = 'TMM Angular2 - ASP.NET!!'; }
