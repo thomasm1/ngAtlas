496332d5481317da8a80ec83e43fefa54127bf43
thomas maestas <thomasm1.maestas@gmail.com>
2019-06-21 16:00:16 -0600

    Routing added

---
+++ angular4-dailytech/src/app/Collection.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'my-app',
+    template: 'Collection'
+})
+export class AppCollection {
+    
+}
--- angular4-dailytech/src/app/app.module.ts
+++ angular4-dailytech/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { Appblogs }  from './blogs.component';
+import { AppCollection }  from './Collection.component';
+import { PageNotFoundComponent } from './NotFound.component';
+
+import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
+
+const appRoutes: Routes = [
+  { path: 'Blogs', component: Appblogs },
+  { path: 'Collection', component: AppCollection },
+  { path: '**', component: PageNotFoundComponent },
+];
+
 @NgModule({
-  imports:      [ BrowserModule, HttpModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, Appblogs, AppCollection, PageNotFoundComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
