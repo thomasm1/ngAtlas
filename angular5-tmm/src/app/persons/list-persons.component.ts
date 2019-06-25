759cf351b2f151e7751ca154e0411df922430f12
thomas maestas <thomasm1.maestas@gmail.com>
2019-06-25 12:48:51 -0600

    search filter fixed; stable point

--- angular5-tmm/src/app/persons/list-persons.component.ts
+++ angular5-tmm/src/app/persons/list-persons.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from '../models/person.model';
 import { PersonService } from './person.service';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
 
 @Component({
   // selector: 'tm-list-persons',
@@ -10,16 +10,32 @@ import { Router } from '@angular/router';
 })
 export class ListPersonsComponent implements OnInit {
   persons: Person[];
-  searchTerm: string;
+  filteredPersons: Person[]; //muyimprtante - no need to query webserver for each filter; returns full list without roundtrip
+
+  private _searchTerm: string;
+  get searchTerm(): string {
+    return this._searchTerm;
+  }  
+
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredPersons = this.filterePersons(value);
+  }
+  filterePersons(searchString: string) {
+    return this.persons.filter(person => person.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
+  //searchTerm: string;
 
   dataFromChild: Person;
   // personToDisplay: Person;
   // private arrayIndex = 1;
   
-  constructor(private _personService: PersonService, private _router: Router) { }
+  constructor(private _personService: PersonService, 
+              private _router: Router) { }
 
   ngOnInit() {
     this.persons = this._personService.getPersons();
+    this.filteredPersons = this.persons;
     //this.personToDisplay = this.persons[0];
   }
   handleNotify(eventData: Person) {
