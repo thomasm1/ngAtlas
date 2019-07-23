59acf5c2eb0167c04d5e45b321f47f35ee801589
thomas maestas <thomasm1.maestas@gmail.com>
2019-07-23 02:04:40 -0600

    angular8: http methods =>post,delete

---
+++ angular8-dailytech/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ApiComponent } from './components/api/api.component';
+import { ApiItemComponent } from './components/api-item/api-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+
+// import { ApiService } from './services/api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ApiComponent,
+    ApiItemComponent,
+    HeaderComponent,
+    AddPostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [], // ApiService
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
